42f357dec8653308a958774fc255c469c450ed79
prateeksrivastav2 <prateeksrivastav703@gmail.com>
2024-04-15 00:52:20 +0530

    l

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+// const Cors=require('cors');
+
 const authRoutes = require('./routes/authRoutes');
 const rideRoutes = require('./routes/rideRoutes'); // Import rideRoutes
 
